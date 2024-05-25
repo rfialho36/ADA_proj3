f3abbfe372a4b531072b74129f8e39ee99699284
tfc-dias <145683079+tfc-dias@users.noreply.github.com>
2024-05-25 16:43:51 +0100

    Tudo certinho

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,64 @@
+/**
+ * @author Rafael Fialho (62945) r.fialho@campus.fct.unl.pt
+ * @author Tomas Dias (63411) tfc.dias@campus.fct.unl.pt
+ * <p>
+ * Lincenciatura em Engenharia Informatica
+ * Faculdade de Ciencias e Tecnologia da Universidade Nova de Lisboa
+ * 2023/2024
+ * <p>
+ * This program solves the problem "Vault Burglary" for the ADA class from the 2nd semester of 2023/2024.
+ * <p>
+ * This is the Main class of the program. It reads the input, creates a VaultBurglarySolver object and prints the
+ * result of the problem.
+ * <p>
+ * Copilot and chatGPT were used to help structuring the solutions logic, simplifying and documenting the code.
+ */
+
+// -------- IMPORTS --------
 import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-            StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
 
-            int numThieves = Integer.parseInt(tokenizer.nextToken());
-            int numGoldBars = Integer.parseInt(tokenizer.nextToken());
-            int numLocations = Integer.parseInt(tokenizer.nextToken());
-            int numRoads = Integer.parseInt(tokenizer.nextToken());
+    /**
+     * This is the main method of the program.
+     * It reads the input, creates a VaultBurglarySolver object and prints the result of the problem.
+     *
+     * @param args The arguments passed to the program.
+     */
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
 
-            VaultBurglarySolver solver = new VaultBurglarySolver(numLocations);
+        // Read the input values (number of thieves, gold bars, locations and roads)
+        int numThieves = Integer.parseInt(tokenizer.nextToken());
+        int numGoldBars = Integer.parseInt(tokenizer.nextToken());
+        int numLocations = Integer.parseInt(tokenizer.nextToken());
+        int numRoads = Integer.parseInt(tokenizer.nextToken());
 
-            // Read the roads and build the graph
-            for (int i = 0; i < numRoads; i++) {
-                tokenizer = new StringTokenizer(reader.readLine());
-                int loc1 = Integer.parseInt(tokenizer.nextToken());
-                int loc2 = Integer.parseInt(tokenizer.nextToken());
-                solver.addRoad(loc1, loc2);
-            }
+        VaultBurglarySolver solver = new VaultBurglarySolver(numThieves, numGoldBars, numLocations, numRoads);
 
-            solver.assignCapacities(numLocations);
-
-            // Read the vault and destination locations
+        // Read the roads and build the graph
+        for (int i = 0; i < numRoads; i++) {
             tokenizer = new StringTokenizer(reader.readLine());
-            int vaultLocation = Integer.parseInt(tokenizer.nextToken());
-            int destinationLocation = Integer.parseInt(tokenizer.nextToken());
-
-            solver.setVaultAndDestination(vaultLocation, destinationLocation);
-
-            // Use the Dinic's algorithm to find the maximum flow
-            int maxFlow = solver.solve(2 * vaultLocation, 2 * destinationLocation + 1, numThieves);
-
-            // Calculate the maximum possible number of stolen gold bars
-            int maxGoldBars = Math.min(numThieves, maxFlow) * numGoldBars;
-
-            // Print the result
-            System.out.println(maxGoldBars);
-        } catch (IOException e) {
-            e.printStackTrace();
+            int loc1 = Integer.parseInt(tokenizer.nextToken());
+            int loc2 = Integer.parseInt(tokenizer.nextToken());
+            solver.addRoad(loc1, loc2);
         }
+
+        // Read the vault and destination locations
+        tokenizer = new StringTokenizer(reader.readLine());
+        int vaultLocation = Integer.parseInt(tokenizer.nextToken());
+        int destinationLocation = Integer.parseInt(tokenizer.nextToken());
+
+        // Set the vault and destination locations
+        solver.setVaultAndDestination(vaultLocation, destinationLocation);
+
+        // Solve the problem and get the result (maximum amount of gold bars that can be stolen)
+        int maxGoldBars = solver.solve();
+
+        // Print the result
+        System.out.println(maxGoldBars);
     }
+
 }
--- src/VaultBurglarySolver.java
+++ src/VaultBurglarySolver.java
@@ -1,100 +1,184 @@
+/**
+ * @author Rafael Fialho (62945) r.fialho@campus.fct.unl.pt
+ * @author Tomas Dias (63411) tfc.dias@campus.fct.unl.pt
+ * <p>
+ * This class solves the problem "Vault Burglary".
+ * It's responsible for building the graph and finding the maximum amount of gold bars that can be stolen.
+ * <p>
+ * Copilot and chatGPT were used to help structuring the solutions logic, simplifying and documenting the code.
+ */
+
+// -------- IMPORTS --------
 import java.util.*;
 
 public class VaultBurglarySolver {
-  private static final int INF = Integer.MAX_VALUE;
 
-  private int[][] capacity;
-  private List<List<Integer>> adj;
-  private int numNodes;
+    // -------- CONSTANTS --------
+    private static final int INF = Integer.MAX_VALUE;
 
-  public VaultBurglarySolver(int numNodes) {
-    this.numNodes = numNodes;
-    this.capacity = new int[2 * numNodes + 2][2 * numNodes + 2];
-    this.adj = new ArrayList<>();
-    for (int i = 0; i <= 2 * numNodes + 1; i++) {
-      adj.add(new ArrayList<>());
-    }
-  }
+    // -------- VARIABLES --------
+    private int[][] capacity;
+    private List<List<Integer>> adj;
+    private int numThieves, numGoldBars, numLocations;
+    private int vaultLocation, destinationLocation;
 
-  public void addRoad(int loc1, int loc2) {
-    int uOut = 2 * loc1 + 1;
-    int vIn = 2 * loc2;
-    int vOut = 2 * loc2 + 1;
-    int uIn = 2 * loc1;
+    // -------- CONSTRUCTOR --------
 
-    capacity[uOut][vIn] = 1; // Edge from loc1 out to loc2 in
-    capacity[vOut][uIn] = 1; // Edge from loc2 out to loc1 in
-
-    adj.get(uOut).add(vIn);
-    adj.get(vOut).add(uIn);
-  }
-
-  public void assignCapacities(int numLocations) {
-    for (int i = 1; i <= numLocations; i++) {
-      capacity[2 * i][2 * i + 1] = INF; // Edge from in-vertex to out-vertex
-      adj.get(2 * i).add(2 * i + 1);
-    }
-  }
-
-  public void setVaultAndDestination(int vaultLoc, int destLoc) {
-    // Vault and destination vertices should have infinite capacity
-    capacity[2 * vaultLoc][2 * vaultLoc + 1] = INF;
-    capacity[2 * destLoc][2 * destLoc + 1] = INF;
-  }
-
-  public int solve(int source, int sink, int thieves) {
-    int[] level = new int[2 * numNodes + 2];
-    int[] ptr = new int[2 * numNodes + 2];
-    int maxFlow = 0;
-
-    while (bfs(source, sink, level)) {
-      Arrays.fill(ptr, 0);
-      int flow;
-      while ((flow = dfs(level, ptr, source, sink, INF)) > 0) {
-        maxFlow += flow;
-        if (maxFlow >= thieves) {
-          return maxFlow;
+    /**
+     * Constructor of the VaultBurglarySolver class.
+     * It initializes the variables of the solver.
+     *
+     * @param numThieves   The number of thieves.
+     * @param numGoldBars  The number of gold bars.
+     * @param numLocations The number of locations.
+     * @param numRoads     The number of roads.
+     */
+    public VaultBurglarySolver(int numThieves, int numGoldBars, int numLocations, int numRoads) {
+        this.numThieves = numThieves;
+        this.numGoldBars = numGoldBars;
+        this.numLocations = numLocations;
+        this.capacity = new int[2 * numLocations + 2][2 * numLocations + 2];
+        this.adj = new ArrayList<>();
+        for (int i = 0; i <= 2 * numLocations + 1; i++) {
+            adj.add(new ArrayList<>());
         }
-      }
+        this.vaultLocation = -1;
+        this.destinationLocation = -1;
+        initializeCapacities(numLocations);
     }
-    return maxFlow;
-  }
 
-  private boolean bfs(int source, int sink, int[] level) {
-    Arrays.fill(level, -1);
-    level[source] = 0;
-    Queue<Integer> queue = new LinkedList<>();
-    queue.add(source);
-
-    while (!queue.isEmpty()) {
-      int u = queue.poll();
-
-      for (int v : adj.get(u)) {
-        if (level[v] < 0 && capacity[u][v] > 0) {
-          level[v] = level[u] + 1;
-          queue.add(v);
+    /**
+     * Initialize capacities and adjacency list for each location.
+     * Each location has a capacity of 1 because only one group can pass through it.
+     *
+     * @param numLocations The number of locations.
+     */
+    private void initializeCapacities(int numLocations) {
+        for (int i = 1; i <= numLocations; i++) {
+            addEdge(2 * i, 2 * i + 1, 1); // Edge from in-vertex to out-vertex
         }
-      }
     }
-    return level[sink] >= 0;
-  }
 
-  private int dfs(int[] level, int[] ptr, int u, int sink, int flow) {
-    if (u == sink) {
-      return flow;
+    /**
+     * Adds a road to the solver.
+     * It adds the road to the graph.
+     *
+     * @param location1 The first location.
+     * @param location2 The second location.
+     */
+    public void addRoad(int location1, int location2) {
+        addEdge(2 * location1 + 1, 2 * location2, 1); // From location1 out to location2 in
+        addEdge(2 * location2 + 1, 2 * location1, 1); // From location2 out to location1 in
     }
-    for (; ptr[u] < adj.get(u).size(); ptr[u]++) {
-      int v = adj.get(u).get(ptr[u]);
-      if (level[v] == level[u] + 1 && capacity[u][v] > 0) {
-        int newFlow = Math.min(flow, capacity[u][v]);
-        int pushed = dfs(level, ptr, v, sink, newFlow);
-        if (pushed > 0) {
-          capacity[u][v] -= pushed;
-          capacity[v][u] += pushed;
-          return pushed;
+
+    /**
+     * Helper method to add an edge to the graph.
+     */
+    private void addEdge(int from, int to, int cap) {
+        capacity[from][to] = cap;
+        adj.get(from).add(to);
+        adj.get(to).add(from); // Add reverse path for residual graph
+    }
+
+    /**
+     * Sets the vault and destination locations.
+     * It sets the vault and destination locations and assigns infinite capacity to them because
+     * all the thieves can (and must) pass through them.
+     *
+     * @param vaultLoc The vault location.
+     * @param destLoc  The destination location.
+     */
+    public void setVaultAndDestination(int vaultLoc, int destLoc) {
+        this.vaultLocation = vaultLoc;
+        this.destinationLocation = destLoc;
+        addEdge(2 * vaultLoc, 2 * vaultLoc + 1, INF); // Infinite capacity for vault
+        addEdge(2 * destLoc, 2 * destLoc + 1, INF); // Infinite capacity for destination
+    }
+
+    /**
+     * Solves the problem.
+     * It uses Dinic's algorithm to find the maximum flow in the graph.
+     * The maximum flow represents the maximum amount of gold bars that can be stolen.
+     * If the maximum flow is greater than or equal to the number of thieves, the result is
+     * the number of thieves times the number of gold bars.
+     *
+     * @return The maximum amount of gold bars that can be stolen.
+     */
+    public int solve() {
+        int source = 2 * vaultLocation;
+        int sink = 2 * destinationLocation + 1;
+        int[] level = new int[2 * numLocations + 2];
+        int[] ptr = new int[2 * numLocations + 2];
+        int maxFlow = 0;
+
+        while (bfs(source, sink, level)) {
+            Arrays.fill(ptr, 0);
+            int flow;
+            while ((flow = dfs(level, ptr, source, sink, INF)) > 0) {
+                maxFlow += flow;
+                if (maxFlow >= numThieves) {
+                    return maxFlow * numGoldBars;
+                }
+            }
         }
-      }
+        return Math.min(numThieves, maxFlow) * numGoldBars;
     }
-    return 0;
-  }
+
+    /**
+     * Breadth-first search algorithm.
+     * It finds the shortest path from the source to the sink.
+     *
+     * @param source The source vertex.
+     * @param sink   The sink vertex.
+     * @param level  The level of each vertex.
+     * @return True if there is a path from the source to the sink, false otherwise.
+     */
+    private boolean bfs(int source, int sink, int[] level) {
+        Arrays.fill(level, -1);
+        level[source] = 0;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(source);
+
+        while (!queue.isEmpty()) {
+            int u = queue.poll();
+
+            for (int v : adj.get(u)) {
+                if (level[v] < 0 && capacity[u][v] > 0) {
+                    level[v] = level[u] + 1;
+                    queue.add(v);
+                }
+            }
+        }
+        return level[sink] >= 0;
+    }
+
+    /**
+     * Depth-first search algorithm.
+     * It finds the maximum flow from the source to the sink.
+     *
+     * @param level The level of each vertex.
+     * @param ptr   The pointer of each vertex.
+     * @param u     The current vertex.
+     * @param sink  The sink vertex.
+     * @param flow  The current flow.
+     * @return The maximum flow from the source to the sink.
+     */
+    private int dfs(int[] level, int[] ptr, int u, int sink, int flow) {
+        if (u == sink) {
+            return flow;
+        }
+        for (; ptr[u] < adj.get(u).size(); ptr[u]++) {
+            int v = adj.get(u).get(ptr[u]);
+            if (level[v] == level[u] + 1 && capacity[u][v] > 0) {
+                int newFlow = Math.min(flow, capacity[u][v]);
+                int pushed = dfs(level, ptr, v, sink, newFlow);
+                if (pushed > 0) {
+                    capacity[u][v] -= pushed;
+                    capacity[v][u] += pushed;
+                    return pushed;
+                }
+            }
+        }
+        return 0;
+    }
 }
